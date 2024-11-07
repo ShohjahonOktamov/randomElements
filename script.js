7c6ed9a9f7514baf68ac2050f13416c0f240f6d0
Shohjahon <hackmaster2108uz@gmail.com>
2024-11-07 15:07:07 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 const limit = document.getElementById("limit");
 const list = document.getElementById("list");
 const generateButton = document.getElementById("generate");
-let numbers = null;
+let numbers = [];
 
 function generate() {
-    if (numbers === null) {
-        numbers = [];
-        for (let i = 1; i <= limit.value; i++) {
-            numbers.push(i);
-        }
+    list.innerHTML = "";
+    numbers = [];
+    for (let i = 1; i <= limit.value; i++) {
+        numbers.push(i);
     }
 
-    if (numbers.length > 0) {
+    while (numbers.length) {
         const randomIndex = Math.floor(Math.random() * numbers.length);
         const number = numbers.splice(randomIndex, 1)[0];
 
         const li = document.createElement("li");
         li.textContent = number;
         list.appendChild(li);
-    } else {
-        alert("No numbers are left!");
     }
 };
